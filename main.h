1b31b5fed91aad2ddbde3b3e8943c16dc450f431
Boubouche1709 <hadjarflorian@gmail.com>
2025-07-21 14:38:11 +0200

    File _putchar.c

--- main.h
+++ main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
