1ffad72767fdfbbd5812b8ddf581acddc3f4ac1d
Boubouche1709 <hadjarflorian@gmail.com>
2025-07-22 10:27:31 +0200

    Add print_percent.c with the function

---
+++ print_percent.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_percent - print the symbol %
+ * @args: list of arguments
+ * Return: symbol % to print
+ */
+int print_percent(va_list args)
+{
+(void)args;
+return (_putchar('%'));
+}
