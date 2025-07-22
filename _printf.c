520c01e259531b72d213aafa30917ca6b7de0f16
Boubouche1709 <hadjarflorian@gmail.com>
2025-07-22 10:29:15 +0200

    Fix the file _printf.c with all create functions

---
+++ _printf.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * _printf - version of printf (handles %c, %s, %%)
+ * @format: format string
+ * Return: number of character to print
+ */
+int _printf(const char *format, ...)
+{
+va_list args;
+int i = 0, count = 0;
+if (!format)
+return (-1);
+va_start(args, format);
+while (format[i])
+{
+if (format[i] == '%' && format[i + 1])
+{
+i++;
+if (format[i] == 'c')
+count += print_char(args);
+else if (format[i] == 's')
+count += print_string(args);
+else if (format[i] == 'd' || format[i] == 'i')
+count += print_int(args);
+else if (format[i] == '%')
+count += print_percent(args);
+else
+{
+count += _putchar('%');
+count += _putchar(format[i]);
+}
+}
+else
+{
+count += _putchar(format[i]);
+}
+i++;
+}
+va_end(args);
+return (count);
+}
